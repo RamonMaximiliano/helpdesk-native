1856bd1fa298c58fdd2202a93f559478b68234df
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-09-18 11:45:57 -0300

    feat: started

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/login';
+
+export default function App() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  );
+}
+
---
+++ src/Login.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Login() {
+    return (
+        <View>
+            <Text style={styles.text}>Hello there!</Text>
+        </View>
+    )
+};
+
+
+const styles = StyleSheet.create({
+      text:{
+        color:"blue",
+      }
+  });
+  
