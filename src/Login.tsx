1f464dcbb6849edd1d6f1fcfdefd4ca7bdc26b0e
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-09-19 16:49:17 -0300

    feat: login started

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { VStack, Heading } from "native-base";
+import React, { useState } from "react";
+import { VStack, Heading, Text, Input, Button } from "native-base";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <VStack flex={1} alignItems="center" bg="gray.800" px={8} pt={24}>
-            <Heading color="black">Hello there! Test</Heading>
+            <AntDesign name="customerservice" size={45} color="white" marginBottom={15} marginTop={15}/>            
+            <Heading color="gray.400" fontSize="3xl" marginBottom={85}>Help<Text color="#7076FE">Desk </Text><Text color="white">Native</Text></Heading>
+            <Text color="white"marginBottom={8} fontSize="lg">Acesse sua conta </Text>
+            <Input placeholder="E-mail" width="xs" fontSize="md" p={3} marginBottom={15} bg="gray.900"/>
+            <Input placeholder="Password" width="xs" fontSize="md" p={3} marginBottom={30}  bg="gray.900"/>
+            <Button background="#7076FE" width="xs" fontSize="lg" p={4}><Text fontSize="md" color="white">Entrar</Text></Button>
         </VStack>
     )
 };
