26293e522698d7d7ea342577f0b40bf4146bd9de
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-10-08 10:26:58 -0300

    feat: register page

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { VStack, Heading, Text} from "native-base";
+import { VStack, Text, Input, Button, HStack } from "native-base";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function New() {
     return (
-        <VStack>
-            <Heading ><Text>New</Text></Heading>
+        <VStack flex={1} bg="gray.900" flexDirection={"column"} px={5} justifyContent="space-between" pb={10}>
+            <HStack display="flex" flexDirection={"row"} alignItems={"center"} color="white" fontSize="xl" h="120" pt={16} justifyContent="space-between" textAlign="center">
+                <AntDesign name="left" size={24} color="white" w="50" />
+                <Text color="white" textAlign="center" fontSize="xl">New ticket</Text>
+                <AntDesign name="right" size={24} color="transparent" w="50" />
+            </HStack>
+            <Input placeholder="Title" h="60" my={2}></Input>
+            <Input placeholder="Description" h="450"></Input>
+            <VStack pt={5} alignItems={"center"} >
+                <Button background="#5960ff" width="full" fontSize="lg" p={4} _pressed={{ bg: "#7076FE" }}><Text fontSize="md" color="white">Create</Text></Button>
+            </VStack>
         </VStack>
     )
 };
